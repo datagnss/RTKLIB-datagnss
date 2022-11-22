73bdc935e6391b72f85740605c63cee758a9c86a
datagnss <39272801+datagnss@users.noreply.github.com>
2022-11-22 14:43:21 +0800

    update

--- app/consapp/rtkrcv/rtkrcv2.c
+++ app/consapp/rtkrcv/rtkrcv2.c
@@ -427,14 +427,14 @@ void gen_cmds()
     sprintf(rov_cmd,"%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n",cmd_1hz,cmd_nmea_disable,cmd_wait_100_ms,cmd_eph_disable,cmd_wait_100_ms,
     cmd_rtcm1005_disable,cmd_wait_100_ms,cmd_msm7_disable,cmd_msm4_disable);
 
-    sprintf(rov_cmd,"%s\n%s\n",cmds,cmd_msm7_enable);
+    sprintf(rov_cmd,"%s\n%s\n",rov_cmd,cmd_msm7_enable);
     
     if (update_rate==1)
-      sprintf(rov_cmd,"%s\n%s\n",cmds,cmd_1hz);
+      sprintf(rov_cmd,"%s\n%s\n",rov_cmd,cmd_1hz);
     else if (update_rate==5)
-      sprintf(rov_cmd,"%s\n%s\n",cmds,cmd_5hz);
+      sprintf(rov_cmd,"%s\n%s\n",rov_cmd,cmd_5hz);
     else if (update_rate==10)
-      sprintf(rov_cmd,"%s\n%s\n",cmds,cmd_10hz);
+      sprintf(rov_cmd,"%s\n%s\n",rov_cmd,cmd_10hz);
     else
       fprintf(stderr,"invalid update rate.\n");
 }
