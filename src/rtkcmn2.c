f9fd71a18e91a416e51adab6e222e7f8fd2753bc
datagnss <39272801+datagnss@users.noreply.github.com>
2022-11-22 23:32:29 +0800

    set outsingle=1

--- src/rtkcmn2.c
+++ src/rtkcmn2.c
@@ -222,7 +222,7 @@ const prcopt_t prcopt_default={ /* defaults processing options */
     {0},{0},{0},                /* baseline,ru,rb */
     {"",""},                    /* anttype */
     {{0}},{{0}},{0},            /* antdel,pcv,exsats */
-    1,1                         /* maxaveep,initrst */
+    1,1,1                         /* maxaveep,initrst,outsingle */
 };
 const solopt_t solopt_default={ /* defaults solution output options */
     SOLF_LLH,TIMES_GPST,1,3,    /* posf,times,timef,timeu */
